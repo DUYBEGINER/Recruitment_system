a71f3015a8db476184e6198e6ab157f9b86dedda
DUYBEGINER <phucduygg8485@gmail.com>
2025-10-26 20:29:40 +0700

    duy push code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Routes, Route, Link } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
+     <Routes>
+      <Route path="/" element={<h1>Home Page</h1>} />
+      <Route path="/about" element={<h1>About Page</h1>} />
+    </Routes>
      
-    </>
+
   )
 }
 
---
+++ src/api/authAPI.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import {axiosClient} from "../utils/axiosClient";
+
+
+const checkSession = async () => {
+  try {
+    // console.log("component call api:", component);
+    const datenow = new Date().toLocaleString();
+    const res = await axiosClient.get("/auth/me");
+    if (res && res.data) {
+      console.log(`Response from checkSession: [${datenow}]`, res.data);
+      return res.data; // Trả về dữ liệu user
+    }
+    return null;
+  } catch (err) {
+    console.error("Error response", err.response);
+    throw new Error(err.response?.data?.message || 'Error checking session');
+  }
+}
+
+// const authRequest = async (idToken) => {
+//    const res = await axiosClient.post(`/auth/login`, {}, {
+//     headers: { Authorization: `Bearer ${idToken}` },
+//   });
+//   console.log("res login:" , res.data)
+//   return res.data;
+// };
+
+
+// const logOutRequest = async (idToken) => {
+//   const res = await axiosClient.post(`/auth/logout`, {}, {
+//     headers: { Authorization: `Bearer ${idToken}` },
+//   });
+//   console.log("res logout:", res)
+//   return res.data;
+// }
+
+export { checkSession };
